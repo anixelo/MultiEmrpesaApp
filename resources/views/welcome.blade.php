8ceaa34a891328d703cfd9b217eb9e12c5947f5b
anixelo <chabivicien@gmail.com>
2026-03-09 08:30:10 +0100

    Añadida empresa a planes de elcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -207,6 +207,10 @@
                         <svg class="w-4 h-4 text-green-500 shrink-0" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"/></svg>
                         {{ ($plan->max_presupuestos ?? 0) == 0 ? 'Presupuestos ilimitados' : 'Hasta ' . $plan->max_presupuestos . ' presupuestos/mes' }}
                     </li>
+                    <li class="flex items-center gap-2 text-sm text-gray-600">
+                        <svg class="w-4 h-4 text-green-500 shrink-0" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"/></svg>
+                        {{ ($plan->max_empresas ?? 0) == 0 ? 'Empresas ilimitadas' : 'Hasta ' . $plan->max_empresas . ' empresas' }}
+                    </li>
                     @if($plan->features)
                         @foreach($plan->features as $feature)
                         <li class="flex items-center gap-2 text-sm text-gray-600">
